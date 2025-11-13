18acbe21d9b0efb9c7d73a181504d445080ab781
Roberto Lepore <roberto.lepore.rl@gmail.com>
2025-11-13 12:26:34 +0100

    main with one hand only

--- main.m
+++ main.m
@@ -2,17 +2,20 @@ clc;
 clear;
 close all;
 
-% path = 'C:\Users\rober\OneDrive\Documents\Uni\Bioinformatics\Neurosciences\project\mef25_V6A\mef25_V6A\';
-path = 'C:\Users\rober\OneDrive\Documents\Uni\Bioinformatics\Neurosciences\project\V6A_mef24\V6A_mef24\';
+% dataset con una sola mano
+path = 'C:\Users\rober\OneDrive\Documents\Uni\Bioinformatics\Neurosciences\project\mef25_V6A\mef25_V6A\';
 
-% % DATASET WITH ONLY ONE HAND
-% principal_angles = full_dpca_analysis(path);
+% dataset con due mani
+% path = 'C:\Users\rober\OneDrive\Documents\Uni\Bioinformatics\Neurosciences\project\V6A_mef24\V6A_mef24\';
+
+% DATASET WITH ONLY ONE HAND
+principal_angles = full_dpca_analysis(path);
 
 % % DATASET WITH TWO HANDS, CONSIDER ONLY ONE HAND
 % principal_angles = full_dpca_analysis(path, 'hand_position', 1);
 % principal_angles = full_dpca_analysis(path, 'hand_position', 2);
 
 % DATASET WITH TWO HANDS, CONSIDER BOTH HANDS
-principal_angles = full_dpca_analysis_both_hands(path);
+% principal_angles = full_dpca_analysis_both_hands(path);
 
 disp(principal_angles)
